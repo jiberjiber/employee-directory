f72cf168c725fe1bfe774d8bf67d3934be042f27
Evan Boswood <evan@boswood.net>
2020-08-29 14:08:34 -0500

    added search form

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -2,7 +2,26 @@ import React from "react";
 import "./style.css";
 
 function Filter(props) {
-	return ();
+	return (
+		<div className="col-md-4 text-center">
+			<form onSubmit={props.handleFormSubmit}>
+				<div className="form-group">
+					<input
+						onChange={props.handleInputChange}
+						value={props.value}
+						name="name"
+						type="text"
+						placeholder="Employee's First Name (Case Sensitive)"
+						id="name"
+						className="form-control"
+					/>
+					<button type="submit" className="btn btn-primary m-2">
+						Search by Name
+					</button>
+				</div>
+			</form>
+		</div>
+	);
 }
 
 export default Filter;
