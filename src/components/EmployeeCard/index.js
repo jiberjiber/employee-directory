b88710fe5671b1b6ad7e13239f7c7da68dd89623
Evan Boswood <evan@boswood.net>
2020-08-29 10:51:41 -0500

    created card

--- src/components/EmployeeCard/index.js
+++ src/components/EmployeeCard/index.js
@@ -2,7 +2,29 @@ import React from "react";
 import "./style.css";
 
 function EmployeeCard(props) {
-	return ();
+	return (
+		<div className="card">
+			<div className="img-container">
+				<img alt={props.name} src={props.img} />
+			</div>
+			<div className="content">
+				<ul>
+					<li>
+						<strong>Name:</strong> {props.name}
+					</li>
+					<li>
+						<strong>Title:</strong> {props.title}
+					</li>
+					<li>
+						<strong>Salary:</strong> {props.salary}
+					</li>
+				</ul>
+			</div>
+			<span onClick={() => props.removeEmployee(props.id)} className="remove">
+				𝘅
+			</span>
+		</div>
+	);
 }
 
 export default EmployeeCard;
