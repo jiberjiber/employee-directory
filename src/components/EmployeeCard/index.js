6a50a6a61c884c5b02cbd362172d53d3db0f3924
Evan Boswood <evan@boswood.net>
2020-08-29 10:27:18 -0500

    Component boilerplate created

---
+++ src/components/EmployeeCard/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.css";
+
+function EmployeeCard(props) {
+	return ();
+}
+
+export default EmployeeCard;
---
+++ src/components/Filter/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.css";
+
+function Filter(props) {
+	return ();
+}
+
+export default Filter;
